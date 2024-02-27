5d0548ec24077d66b5b188e4eeb067a8e1e6874c
BeinAnEngg <157193559+BeinAnEngg@users.noreply.github.com>
2024-02-27 21:08:29 +0530

    1.c

--- 1.c
+++ 1.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <sys/wait.h>  // Include sys/wait.h for wait()
+#include <sys/wait.h>  
 #include <unistd.h>
 
 int main() {
     pid_t pid;
     
-    // Create a child process using fork()
+   
     pid = fork();
     if (pid < 0) {
-        // Fork failed
+        
         perror("Fork failed");
         exit(1);
     } else if (pid == 0) {
-        // Child process
+      
         printf("Child process with PID %d\n", getpid());
         execlp("/bin/ls", "ls", NULL);
-        perror("exec failed"); // Print an error if execlp fails
+        perror("exec failed");
         exit(1);
     } else {
-        // Parent process
-        wait(NULL);  // Wait for the child process to complete
+      
+        wait(NULL);  
         printf("Child Complete\n");
     }
     return 0;
