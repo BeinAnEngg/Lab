3206bec6a258f5899223f4b86fbc8e6f57ee8d67
BeinAnEngg <157193559+BeinAnEngg@users.noreply.github.com>
2024-02-28 00:51:12 +0530

    1.c

--- 1.c
+++ 1.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <sys/wait.h>  
+#include <sys/wait.h>
 #include <unistd.h>
 
-int main() {
+int main()
+{
     pid_t pid;
-    
-   
     pid = fork();
-    if (pid < 0) {
-        
+    if (pid < 0)
+    {
         perror("Fork failed");
         exit(1);
-    } else if (pid == 0) {
-      
-        printf("Child process with PID %d\n", getpid());
-        execlp("/bin/ls", "ls", NULL);
-        perror("exec failed");
-        exit(1);
-    } else {
-      
-        wait(NULL);  
-        printf("Child Complete\n");
     }
-    return 0;
+    else if (pid == 0)
+    {       
+        printf("Child process with PID %d\n", getpid());
+        execlp("/bin/ls","ls",NULL);
+    }
+    else
+    {
+        wait (NULL);
+        printf("Child Complete");
+    }
 }
