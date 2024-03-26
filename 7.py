e4eeb077d87cb5ef6f01d830fd63ab32757da172
BeinAnEngg <157193559+BeinAnEngg@users.noreply.github.com>
2024-03-26 06:50:55 +0530

    7.py

--- 7.py
+++ 7.py
@@ -1,10 +1,10 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
-# Load a built-in dataset from Seaborn
+# built-in dataset from Seaborn
 tips = sns.load_dataset("tips")
 # Set Seaborn style and color palette
 sns.set(style="whitegrid", palette="Set1")
-# Create a customized scatter plot
+# Scatter plot
 plt.figure(figsize=(8, 6))
 sns.scatterplot(x="total_bill", y="tip", data=tips, hue="time", style="sex", 
 s=100, palette="Set2")
@@ -13,7 +13,7 @@ plt.xlabel("Total Bill")
 plt.ylabel("Tip")
 plt.legend(title="Time of Day")
 plt.show()
-# Create a customized histogram
+# Histogram
 plt.figure(figsize=(8, 6))
 sns.histplot(tips["total_bill"], bins=20, kde=True, color="skyblue", 
 line_kws={"color": "red"})
@@ -22,7 +22,7 @@ plt.xlabel("Total Bill")
 plt.ylabel("Frequency")
 plt.grid(True)
 plt.show()
-# Create a customized box plot
+# Box plot
 plt.figure(figsize=(8, 6))
 sns.boxplot(x="day", y="total_bill", data=tips, palette="husl")
 plt.title("Customized Box Plot")
